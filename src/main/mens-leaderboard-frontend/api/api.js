b21d7edfc70388567d2efb650f97a7f2f2d17fa2
Danislav Anufriev <83768491+Dels1x@users.noreply.github.com>
2024-03-09 18:53:33 +0200

    switched reverse proxy

--- src/main/mens-leaderboard-frontend/api/api.js
+++ src/main/mens-leaderboard-frontend/api/api.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const size = 50;
-const mainHttp = "https://good-squids-dig.loca.lt";
+const mainHttp = "https://blue-mice-grab.loca.lt";
 const headers = new Headers();
 headers.append("bypass-tunnel-reminder", process.env.TUNNEL_PASSWORD);
 
